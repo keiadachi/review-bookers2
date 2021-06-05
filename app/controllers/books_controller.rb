e6a1ff507453606e5c2113f25aade3abb5655520
keiadachi <2013edcckeiadachi@gmail.com>
2021-06-05 07:34:19 +0000

    book投稿修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,55 +1,39 @@
 class BooksController < ApplicationController
 
-  before_action :ensure_correct_user
 
-
-  def show
-    @book = Book.find(params[:id])
-    @book_comment = BookComment.new
+  def new
   end
 
+
   def index
-    @books = Book.all
-    @book = Book.new
+    @books = Book.new
+  end
+
+  def show
+  end
+
+  def edit
   end
 
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      redirect_to book_path(@book), notice: "You have created book successfully."
+      redirect_to user_path(@book), notice: "You have created book successfully."
     else
       @books = Book.all
       render 'index'
     end
   end
 
-  def edit
-  end
 
-  def update
-    if @book.update(book_params)
-      redirect_to book_path(@book), notice: "You have updated book successfully."
-    else
-      render "edit"
-    end
-  end
 
-  def destroy
-    @book.destroy
-    redirect_to books_path
-  end
+
+
 
   private
-
   def book_params
-    params.require(:book).permit(:title, :body)
+    params.require(:book).permit(:title, :opinion, :user_id)
   end
 
-  def ensure_correct_user
-    @book = Book.find(params[:id])
-    unless @book.user == current_user
-      redirect_to books_path
-    end
-  end
 end
